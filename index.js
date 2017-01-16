24cf2391a2c782dd2f4b320643b3d6680781b25a
rajanshahsa <rajan.shah.sa@gmail.com>
2017-01-16 15:32:06 +0530

    Change var to let

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
+let express = require("express");
+let bodyParser = require("body-parser");
+let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var server = app.listen(3000, function () {
+let server = app.listen(3000, function () {
     console.log("Listening on port %s...", server.address().port);
 });
 
@@ -21,24 +21,23 @@ app.post("/user/signUp", function (req, res) {
     if (!req.body.username || !req.body.password || !req.body.sex || !req.body.dob || !req.body.mobileNo || !req.body.emailId) {
         return res.send({ "status": "error", "message": "missing a parameter" });
     } else {
-        var jwt = require('jsonwebtoken');
+        let jwt = require('jsonwebtoken');
         // Retrieve
-        var MongoClient = require('mongodb').MongoClient;
+        let MongoClient = require('mongodb').MongoClient;
         // Connect to the db
         MongoClient.connect("mongodb://127.0.0.1:27017/EMS", function (err, db) {
             if (!err) {
-                var collection = db.collection('User');
+                let collection = db.collection('User');
                 //Create some users
-                var token = jwt.sign(req.body.emailId, "secret");
-                console.log(token);
-                var user1 = { username: req.body.username, password: req.body.password, sex: req.body.sex, dob: req.body.dob, mobileNo: req.body.mobileNo, emailId: req.body.emailId };
+                let token = jwt.sign(req.body.emailId, "secret");
+                let user1 = { username: req.body.username, password: req.body.password, sex: req.body.sex, dob: req.body.dob, mobileNo: req.body.mobileNo, emailId: req.body.emailId };
 
                 // Insert some users
                 collection.insert([user1], function (err, result) {
                     if (err) {
                         console.log(err);
                     } else {
-                        var data = {
+                        let data = {
                             userId: result._id,
                             username: req.body.username,
                             emailId: req.body.emailId,
@@ -46,14 +45,13 @@ app.post("/user/signUp", function (req, res) {
                             dob: req.body.dob,
                             mobileNo: req.body.mobileNo,
                         }
-                        var body = {
+                        let body = {
                             xAuthToken: token,
                             data: data,
                             message: "valid user",
                             status: 1
                         }
                         return res.send(body)
-                        console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
                     }
                     //Close connection
                     db.close();
@@ -82,35 +80,31 @@ app.post("/user/login", function (req, res) {
     if (!req.body.emailId || !req.body.password) {
         return res.send({ "status": "error", "message": "missing a parameter" });
     } else {
-        console.log("Authenticate");
-        var jwt = require('jsonwebtoken');
-        var token = jwt.sign(req.body.emailId, "secret");
-        console.log(token);
+        let jwt = require('jsonwebtoken');
+        let token = jwt.sign(req.body.emailId, "secret");
         // Retrieve
-        var MongoClient = require('mongodb').MongoClient;
+        let MongoClient = require('mongodb').MongoClient;
         // Connect to the db
         MongoClient.connect("mongodb://127.0.0.1:27017/EMS", function (err, db) {
             if (!err) {
-                var EMS = db.collection('User');
-                var result = EMS.find({ $and: [{ emailId: req.body.emailId }, { password: req.body.password }] }).toArray(function (err, result) {
+                let EMS = db.collection('User');
+                let result = EMS.find({ $and: [{ emailId: req.body.emailId }, { password: req.body.password }] }).toArray(function (err, result) {
                     if (err) {
-                        var body = {
+                        let body = {
                             message: "Error occurred",
                             status: 0
                         }
                         db.close();
                         return res.send(body)
-
-                        console.log(err);
                     } else if (result.length) {
-                        var data = {
+                        let data = {
                             username: result[0].username,
                             emailId: result[0].emailId,
                             sex: result[0].sex,
                             dob: result[0].dob,
                             mobileNo: result[0].mobileNo
                         }
-                        var body = {
+                        let body = {
                             xAuthToken: token,
                             data: data,
                             message: "Success",
@@ -119,14 +113,12 @@ app.post("/user/login", function (req, res) {
                         db.close();
                         return res.send(body)
                     } else {
-                        var body = {
+                        let body = {
                             message: "Invalid Credentials",
                             status: 0
                         }
                         db.close();
                         return res.send(body)
-
-                        console.log('No document(s) found with defined "find" criteria!');
                     }
                     //Close connection
                     db.close();
@@ -138,69 +130,76 @@ app.post("/user/login", function (req, res) {
             }
         });
     }
+
 });
 
 app.get("/user/getUser", function (req, res) {
-    // Retrieve
-    var MongoClient = require('mongodb').MongoClient;
-    // Connect to the db
-    MongoClient.connect("mongodb://127.0.0.1:27017/EMS", function (err, db) {
-        if (!err) {
-            var EMS = db.collection('User');
-            var result = EMS.find({}).toArray(function (err, result) {
-                if (err) {
-                    var body = {
-                        message: "Error occurred",
-                        status: 0
-                    }
-                    db.close();
-                    return res.send(body)
-
-                    console.log(err);
-                } else if (result.length) {
-                    var token = req.get("xAuthToken")
-                    var jwt = require('jsonwebtoken');
-                    var decoded = jwt.decode(token, { complete: true });
-                    var userArray = []
-                    for (var i = 0; i < result.length; i++) {
-                        if (decoded.payload !== result[i].emailId) {
-                            var data = {
-                                username: result[i].username,
-                                emailId: result[i].emailId,
-                                sex: result[i].sex,
-                                dob: result[i].dob,
-                                mobileNo: result[i].mobileNo
+    if (req.get("xAuthToken")) {
+        // Retrieve
+        let MongoClient = require('mongodb').MongoClient;
+        // Connect to the db
+        MongoClient.connect("mongodb://127.0.0.1:27017/EMS", function (err, db) {
+            if (!err) {
+                let EMS = db.collection('User');
+                let result = EMS.find({}).toArray(function (err, result) {
+                    if (err) {
+                        let body = {
+                            message: "Error occurred",
+                            status: 0
+                        }
+                        db.close();
+                        return res.send(body)
+                    } else if (result.length) {
+                        let token = req.get("xAuthToken")
+                        let jwt = require('jsonwebtoken');
+                        let decoded = jwt.decode(token, { complete: true });
+                        let userArray = []
+                        for (let i = 0; i < result.length; i++) {
+                            if (decoded.payload !== result[i].emailId) {
+                                let data = {
+                                    username: result[i].username,
+                                    emailId: result[i].emailId,
+                                    sex: result[i].sex,
+                                    dob: result[i].dob,
+                                    mobileNo: result[i].mobileNo
+                                }
+                                userArray.push(data);
                             }
-                            userArray.push(data);
+
+
                         }
 
-
+                        let body = {
+                            data: userArray,
+                            message: "Success",
+                            status: 1
+                        }
+                        db.close();
+                        return res.send(body)
+                    } else {
+                        let body = {
+                            message: 'No user found',
+                            status: 0
+                        }
+                        db.close();
+                        return res.send(body)
                     }
-
-                    var body = {
-                        data: userArray,
-                        message: "Success",
-                        status: 1
-                    }
+                    //Close connection
                     db.close();
-                    return res.send(body)
-                } else {
-                    var body = {
-                        message: 'No user found',
-                        status: 0
-                    }
-                    db.close();
-                    return res.send(body)
+                });
 
-                    console.log('No document(s) found with defined "find" criteria!');
-                }
-                //Close connection
-                db.close();
-            });
+            }
+            else {
+                console.log(err)
+            }
+        });
+    }
+    else {
+        let body = {
+            message: "Unauthorised Request",
+            status: 0
+        }
+        return res.send(body)
+    }
 
-        }
-        else {
-            console.log(err)
-        }
-    });
 });
